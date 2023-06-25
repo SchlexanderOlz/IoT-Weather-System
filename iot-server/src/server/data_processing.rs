bfcdb828a8e039db1c573cfe3f1ad4a4ed6a0370
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-06-25 19:36:37 +0200

    Added setup for temporary caching

--- iot-server/src/server/data_processing.rs
+++ iot-server/src/server/data_processing.rs
@@ -1,6 +1,8 @@
 use mongodb::{bson::doc, Client, Collection};
 use serde::{Deserialize, Serialize};
-use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::{error::Error};
 
 #[derive(Serialize, Deserialize)]
 pub struct SensorData {
@@ -11,25 +13,62 @@ pub struct SensorData {
     light_level: Option<f32>,
 }
 
+
 pub struct DataProcessor {
-    collection: Collection<SensorData>,
+    collection: Option<Collection<SensorData>>,
+    cache_file: File
 }
 
 impl DataProcessor {
     pub async fn new() -> Result<DataProcessor, Box<dyn Error>> {
-        let client = Client::with_uri_str("mongodb://localhost:27017").await?;
-        let db = client.database("IoT-DB");
-         db.run_command(doc! {"create": "SensorData"}, None)
-            .await?;
-        let collection = db.collection("SensorData");
-
-        Ok(DataProcessor { collection })
+        Ok(DataProcessor { collection: DataProcessor::connect().await, cache_file: File::create("cache.json")? })
     }
 
-    pub async fn insert(&self, data: SensorData) -> Result<(), Box<dyn Error>> {
-        self.collection.insert_one(data, None)
+    async fn connect() -> Option<Collection<SensorData>> {
+        let client = match Client::with_uri_str("mongodb://localhost:27017").await {
+            Ok(client) => client,
+            Err(_) => return None
+        };
+
+        let db = client.database("IoT-DB");
+        if db.run_command(doc! {"create": "SensorData"}, None).await.is_err() {
+            println!("[*]Tables already exist")
+        }
+        Some(db.collection("SensorData"))
+    }
+
+
+    pub async fn insert(&mut self, data: SensorData) -> Result<(), Box<dyn Error>> {
+        if let Some(collection) = &self.collection {
+            todo!("Add inserting of json cache here");
+            if let Err(err) = self.insert_mongodb(&collection, data).await {
+                self.collection = None;
+                return Err(err)
+            }
+            Ok(())
+        } else {
+            self.collection = DataProcessor::connect().await;
+            self.insert_cache(data)
+        }
+    }
+
+    async fn insert_mongodb(&self, collection: &Collection<SensorData>, data: SensorData) -> Result<(), Box<dyn Error>> {
+        collection.insert_one(data, None)
         .await
         .map(|_| ())
         .map_err(|err| Box::new(err) as Box<dyn Error>)
+    }
+
+    fn insert_cache(&self, data: SensorData) -> Result<(), Box<dyn Error>> {
+        let json = serde_json::to_string(&data)?;
+        writeln!(self.cache_file, "{}", json)?;
+        Ok(())
+    }
+
+    fn send_cache_to_db(&self) {
+        let buf;
+        self.cache_file.read_to_end(buf);
+        
+        todo!("Implement sending of cache")
     }
 }
